52e8f723258629b60ad7be6962e1d2621ecd7455
dwong28 <dwong28@calpoly.edu>
2016-12-12 17:31:55 -0800

    Implemented Distance()
    Added import for Math Methods.
    Should be the correct implementation of euclidian distance.

--- Vector.java
+++ Vector.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.lang.Math;
 public class Vector implements Comparable<Vector> {
 	ArrayList<Integer> points;
 	Integer classifier;
@@ -15,7 +15,13 @@ public class Vector implements Comparable<Vector> {
 	
 	/*Write a function that takes in a vector and returns the distance.*/
 	public void computeDistance(ArrayList<Integer> vectorB) {
-		
+		double dist = 0;
+		for (int i = 0; i < vectorB.size(); i++) {
+			double temp = this.points.get(i) - vectorB.get(i);
+			dist += Math.pow(temp, 2);
+		}
+
+		return Math.sqrt(dist);
 	}
 	
 	/*Implement a comparator on distance*/
